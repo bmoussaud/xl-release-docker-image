ddabe968c8672f0e8496efd58a9ec9bcbcba8d28
Benoit Moussaud <bmoussaud@xebialabs.com>
2018-11-27 15:38:54 +0100

    update yaml file and build

--- build_image.sh
+++ build_image.sh
@@ -1,1 +1,1 @@
-docker build --build-arg XLR_VERSION=8.2.0 --tag bmoussaud/xl-release-with-db:8.2.0  -f debian-slim/Dockerfile .
+docker build --build-arg XLR_VERSION=8.2.0 --tag bmoussaud/xl-release-with-db:8.2.1  -f debian-slim/Dockerfile .
